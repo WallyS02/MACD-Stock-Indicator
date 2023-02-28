6f57f2e56cab554ee95e5f8f913446f6ebeb93c6
Sebastian Kutny <sebkutny@gmail.com>
2023-02-28 18:51:14 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import pandas
+import jupyter
+
+
+def EMA():
+    print()
+
+
+def MACD():
+    print()
+
+
+def SIGNAL():
+    print()
+
+
+def input():
+    print()
+
+
+def output():
+    print()
+
+
+def main():
+    print()
+
+
+if __name__ == '__main__':
+    main()
