bb49d6984912a1fbba886dc2368f022b145b8693
Sebastian Kutny <sebkutny@gmail.com>
2023-02-28 23:44:54 +0100

    Starting

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
-import pandas
+import pandas as pd
 import jupyter
+import os
 
 
-def EMA():
-    print()
+def EMA(samples, periods, today):
+    alpha = 2/(periods+1)
+    alphaCalc = 1 - alpha
+    nominator = samples[today]
+    denominator = 1.0
+    for i in range(0, periods):
+        denominator = denominator + pow(alphaCalc, i)
+        nominator = nominator + pow(alphaCalc, i) * samples[i+today]
+    return nominator / denominator
 
 
-def MACD():
-    print()
+def MACD(samples, today):
+    EMA_12 = EMA(samples, 12, today)
+    EMA_26 = EMA(samples, 26, today)
+    return EMA_12 - EMA_26
 
 
-def SIGNAL():
-    print()
+def SIGNAL(MACDsamples, index):
+    return EMA(MACDsamples, 9, index)
 
 
-def input():
-    print()
+def CSVinput():
+    path = os.path.dirname(os.path.abspath(__file__))
+    path = path + "\\Quotes\\"
+    #filename = input('Input file name to process it: ')
+    filename = 'Dogecoin'
+    path = path + filename
+    path = path + '.csv'
+    samples = pd.read_csv(path, usecols=['Zamkniecie'], nrows=1000)
+    return samples
 
 
 def output():
@@ -23,7 +40,9 @@ def output():
 
 
 def main():
-    print()
+    samples = CSVinput()
+    MACDsamples = []
+    SIGNALsamples = []
 
 
 if __name__ == '__main__':
