e31da9882f45b30b5f076419635355e8c7eef9c7
Sebastian Kutny <97989092+WallyS02@users.noreply.github.com>
2023-03-01 11:58:10 +0100

    Add report and fix plots.

--- main.py
+++ main.py
@@ -1,48 +1,63 @@
 import pandas as pd
-import jupyter
+import matplotlib.pyplot as plt
 import os
 
 
-def EMA(samples, periods, today):
+def EMA(samples, periods, today, isDataFrame):
     alpha = 2/(periods+1)
     alphaCalc = 1 - alpha
-    nominator = samples[today]
-    denominator = 1.0
+    nominator = 0.0
+    denominator = 0.0
     for i in range(0, periods):
         denominator = denominator + pow(alphaCalc, i)
-        nominator = nominator + pow(alphaCalc, i) * samples[i+today]
+        if isDataFrame is True:
+            nominator = nominator + pow(alphaCalc, i) * samples.loc[i+today]["Zamkniecie"]
+        else:
+            nominator = nominator + pow(alphaCalc, i) * samples[i+today]
     return nominator / denominator
 
 
 def MACD(samples, today):
-    EMA_12 = EMA(samples, 12, today)
-    EMA_26 = EMA(samples, 26, today)
+    EMA_12 = EMA(samples, 12, today, True)
+    EMA_26 = EMA(samples, 26, today, True)
     return EMA_12 - EMA_26
 
 
 def SIGNAL(MACDsamples, index):
-    return EMA(MACDsamples, 9, index)
+    return EMA(MACDsamples, 9, index, False)
 
 
 def CSVinput():
     path = os.path.dirname(os.path.abspath(__file__))
     path = path + "\\Quotes\\"
     #filename = input('Input file name to process it: ')
-    filename = 'Dogecoin'
+    filename = 'CD project'
     path = path + filename
     path = path + '.csv'
-    samples = pd.read_csv(path, usecols=['Zamkniecie'], nrows=1000)
+    samples = pd.read_csv(path, usecols=['Zamkniecie'], nrows=1035)
     return samples
 
 
-def output():
-    print()
+def output(MACDsamples, SIGNALsamples):
+    plt.style.use('_mpl-gallery')
+    fig, ax = plt.subplots()
+    x = [i for i in range(0, 1000)]
+    for i in range(0, 9):
+        MACDsamples.pop()
+    ax.plot(x, MACDsamples, linewidth=2.0)
+    ax.plot(x, SIGNALsamples, linewidth=2.0)
+    plt.show()
 
 
 def main():
     samples = CSVinput()
     MACDsamples = []
     SIGNALsamples = []
+    for i in range(0, 1009):
+        MACDsamples.insert(i, MACD(samples, i))
+    for i in range(0, 1000):
+        SIGNALsamples.insert(i, SIGNAL(MACDsamples, i))
+    output(MACDsamples, SIGNALsamples)
 
 
 if __name__ == '__main__':
