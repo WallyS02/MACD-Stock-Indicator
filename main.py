624e4f8f6aa634cc6e7a5146661fe47302fd3750
Sebastian Kutny <sebkutny@gmail.com>
2023-03-03 20:32:52 +0100

    Final version, eventually minor fixes.

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 import os
 import matplotlib
 
+SAMPLES_NUMBER = 1000
+ALL_SAMPLES_NUMBER = SAMPLES_NUMBER + 100
+MACD_SAMPLES_NUMBER = SAMPLES_NUMBER + 9
+
 
 def EMA(samples, periods, today, isDataFrame):
     alpha = 2 / (periods + 1)
@@ -32,56 +36,107 @@ def SIGNAL(MACDsamples, index):
 def CSVinput():
     path = os.path.dirname(os.path.abspath(__file__))
     path = path + "\\Quotes\\"
-    # filename = input('Input file name to process it: ')
-    filename = 'CD projekt'
+    filename = input('Input file name to process it: ')
     path = path + filename
     path = path + '.csv'
-    samples = pd.read_csv(path, usecols=['Zamkniecie', 'Data'], nrows=1035)
-    #samples = pd.read_csv(path)
-    #samples[::-1].to_csv("Quotes/CD projekt.csv")
+    samples = pd.read_csv(path, usecols=['Zamkniecie', 'Data', 'Najwyzszy', 'Najnizszy', 'Wolumen'],
+                          nrows=ALL_SAMPLES_NUMBER)
     return samples
 
 
-def output(MACDsamples, SIGNALsamples, samples):
+def output(MACDsamples, SIGNALsamples, samples, MoneyFlowSamples):
     matplotlib.use("TkAgg")
     plt.style.use('ggplot')
     x = []
-    for i in range(0, 1000):
+    for i in range(0, SAMPLES_NUMBER):
         splittedDate = str(samples.loc[i]['Data']).split('-')
         date = datetime(int(splittedDate[0]), int(splittedDate[1]), int(splittedDate[2]))
         x.append(date)
-    # x = [i for i in range(0, 1000)]
-    for i in range(0, 9):
-        MACDsamples.pop()
-    plt.plot(x, MACDsamples, linewidth=2.0)
-    plt.plot(x, SIGNALsamples, linewidth=2.0)
+    plt.plot(x, MACDsamples, linewidth=1.0)
+    plt.plot(x, SIGNALsamples, linewidth=1.0)
     plt.title("wykres wskaźnika MACD/SIGNAL")
-    plt.xlabel("dni próbek")
+    plt.xlabel("dni")
     plt.ylabel("wartości wskaźnika")
     plt.legend(["MACD", "SIGNAL"])
     plt.tight_layout()
     plt.show()
     samplesArray = []
-    for i in range(0, 1000):
+    for i in range(0, SAMPLES_NUMBER):
         samplesArray.append(samples.loc[i]["Zamkniecie"])
-    plt.plot(x, samplesArray, linewidth=2.0)
-    plt.xlabel("dni próbek")
-    plt.ylabel("wartości zamknięcia")
+    plt.plot(x, samplesArray, linewidth=1.0)
+    plt.xlabel("dni")
+    plt.ylabel("wartości ceny zamknięcia")
     plt.legend(["Dane"])
     plt.title("wykres danych akcji firmy CD project")
     plt.tight_layout()
     plt.show()
+    plt.plot(x, MoneyFlowSamples, linewidth=1.0)
+    plt.xlabel("dni")
+    plt.ylabel("wartości wskaźnika")
+    plt.legend(["Money Flow"])
+    plt.title("wykres wskaźnika Money Flow")
+    plt.tight_layout()
+    plt.show()
+
+
+def MoneyFlow(samples, today, periods):
+    lastTypical = 0.0
+    positiveMoneyFlow = 0.0
+    negativeMoneyFlow = 0.0
+    for i in range(0, periods):
+        typical = (samples.loc[i + today]["Zamkniecie"] + samples.loc[i + today]["Najwyzszy"] + samples.loc[i + today][
+            "Najnizszy"]) / 3
+        moneyFlow = typical * samples.loc[i + today]["Wolumen"]
+        if typical > lastTypical:
+            positiveMoneyFlow = positiveMoneyFlow + moneyFlow
+        else:
+            negativeMoneyFlow = negativeMoneyFlow + moneyFlow
+        lastTypical = typical
+    moneyRatio = positiveMoneyFlow / negativeMoneyFlow
+    mfi = 100 - (100 / (1+moneyRatio))
+    return mfi
+
+
+def simulation(MACDsamples, SIGNALsamples, samples, MoneyFlowSamples):
+    capital = 1000.0
+    actions = 0.0
+    lastMACD = float()
+    lastSIGNAL = float()
+    for i in range(SAMPLES_NUMBER - 1, -1, -1):
+        if MoneyFlowSamples[i] > 80:
+            capital = capital + (actions * samples.loc[i]["Zamkniecie"])
+            actions = 0
+        if MoneyFlowSamples[i] < 20:
+            actions = actions + (capital / samples.loc[i]["Zamkniecie"])
+            capital = 0
+        if lastMACD > MACDsamples[i] > SIGNALsamples[i] > lastSIGNAL:
+            capital = capital + (actions * samples.loc[i]["Zamkniecie"])
+            actions = 0
+        if lastMACD < MACDsamples[i] < SIGNALsamples[i] < lastSIGNAL:
+            actions = actions + (capital / samples.loc[i]["Zamkniecie"])
+            capital = 0
+        lastMACD = MACDsamples[i]
+        lastSIGNAL = SIGNALsamples[i]
+    if actions != 0.0:
+        capital = capital + actions * samples.loc[SAMPLES_NUMBER - 1]["Zamkniecie"]
+    print(capital)
 
 
 def main():
     samples = CSVinput()
     MACDsamples = []
     SIGNALsamples = []
-    for i in range(0, 1009):
+    MoneyFlowSamples = []
+    for i in range(0, MACD_SAMPLES_NUMBER):
         MACDsamples.insert(i, MACD(samples, i))
-    for i in range(0, 1000):
+    for i in range(0, SAMPLES_NUMBER):
         SIGNALsamples.insert(i, SIGNAL(MACDsamples, i))
-    output(MACDsamples, SIGNALsamples, samples)
+    for i in range(0, 9):
+        MACDsamples.pop()
+    for i in range(0, SAMPLES_NUMBER):
+        MoneyFlowSamples.insert(i, MoneyFlow(samples, i, 14))
+    simulation(MACDsamples, SIGNALsamples, samples, MoneyFlowSamples)
+    output(MACDsamples, SIGNALsamples, samples, MoneyFlowSamples)
 
 
 if __name__ == '__main__':
